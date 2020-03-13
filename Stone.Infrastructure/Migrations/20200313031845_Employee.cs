7a79569e1a0d3e7c6abcb5d6494d61b98e137c18
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-03-13 00:25:36 -0300

    correcao da classe employee para utilizacao de Migrations

---
+++ Stone.Infrastructure/Migrations/20200313031845_Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stone.Infrastructure.Migrations
+{
+    public partial class Employee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    SobreNome = table.Column<string>(nullable: true),
+                    Cpf = table.Column<string>(nullable: true),
+                    Setor = table.Column<string>(nullable: true),
+                    SalarioBruto = table.Column<long>(nullable: false),
+                    Admissao = table.Column<DateTime>(nullable: false),
+                    PlanoSaude = table.Column<bool>(nullable: false),
+                    PlanoDental = table.Column<bool>(nullable: false),
+                    ValeTransporte = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Id", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
