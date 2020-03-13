308afbf55aab7460527b1a2b7f7138de0e008a94
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-03-12 23:25:29 -0300

    Criado arquivo de conexao com banco de dados

---
+++ Stone.Infrastructure/DataContextLayer/EFDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stone.Infrastructure.DataContextLayer
+{
+    class EFDataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+        }
+    }
+}
