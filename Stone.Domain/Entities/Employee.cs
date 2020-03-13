d3b8bdcd8ad54860ed08a5ece22fffc07f6bcfcb
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-03-12 23:38:04 -0300

    entidade Employee

---
+++ Stone.Domain/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stone.Domain.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Cpf { get; set; }
+        public string Setor { get; set; }
+        public long SalarioBruto { get; set; }
+        public DateTime Admissao { get; set; }
+        public bool PlanoSaude { get; set; }
+        public bool PlanoDental { get; set; }
+        public bool ValeTransporte { get; set; }
+    }
+}
