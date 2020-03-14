bb872822baa3bce2ddb651d57f734289cb0ef159
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-03-14 00:48:29 -0300

    servico para salvar e buscar por id - employee

---
+++ Stone.Service/EmployeeService.cs
@@ -0,0 +1,28 @@
+﻿using Stone.Domain.Entities;
+using Stone.Domain.Interface.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stone.Service
+{
+    public class EmployeeService
+    {
+        private readonly IEmployee _employeeRepository;
+
+        public EmployeeService(IEmployee employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            _employeeRepository.Create(employee);
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            return _employeeRepository.Read(id);
+        }
+    }
+}
