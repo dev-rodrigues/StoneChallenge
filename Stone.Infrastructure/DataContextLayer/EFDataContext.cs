2da6d228fff6f3e344908d7888a32397b1af7eae
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-03-15 18:20:51 -0300

    config deploy

--- Stone.Infrastructure/DataContextLayer/EFDataContext.cs
+++ Stone.Infrastructure/DataContextLayer/EFDataContext.cs
@@ -14,7 +14,7 @@ namespace Stone.Infrastructure.DataContextLayer
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer(@"Server=tcp:stone-pay-db.database.windows.net,1433;Initial Catalog=stonepay-db;Persist Security Info=False;User ID=carlos;Password=segredo.3#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
