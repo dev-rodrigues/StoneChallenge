a27b582c853bb1bbb73ce7e8242ec8e87d201a0a
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-03-18 08:49:13 -0300

    removendo confi porta inicializacao

--- Stone.API/Program.cs
+++ Stone.API/Program.cs
@@ -19,6 +19,7 @@ namespace Stone.API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:5001/")
                 .UseStartup<Startup>();
     }
 }
